e39fbda2719dc66e0e4c0738c7d567bde7a7cd30
Orel Cosseron <orel.cosseron@ens-lyon.fr>
2022-11-04 10:37:13 +0100

    Tiles are self contained

--- maze.py
+++ maze.py
@@ -1,9 +1,8 @@
 from tiles import Tile
 from block import Block
 from player import Player
-from exit import Exit
 from PySide6.QtWidgets import (
-    QGridLayout, QWidget, QGraphicsView, QGraphicsScene)
+    QHBoxLayout, QWidget, QGraphicsView, QGraphicsScene)
 from PySide6.QtGui import QPixmap
 from PySide6.QtCore import Qt, Signal, Slot
 
@@ -16,11 +15,11 @@ class Maze(QWidget):
     def __init__(self):
         QWidget.__init__(self)
 
-        self.layout = QGridLayout(self)
+        self.layout = QHBoxLayout(self)
         self.layout.setSpacing(0)
         self.scene = QGraphicsScene()
         self.view = QGraphicsView(self.scene)
-        self.layout.addWidget(self.view, 0, 0)
+        self.layout.addWidget(self.view)
 
         self.tiles = []
         self.blocks = {}
@@ -40,11 +39,9 @@ class Maze(QWidget):
 
             if line[:8] == "TELEPORT":
                 bits = line.split(" ")
-                tile = self.tiles[int(bits[1])][int(bits[2])]
                 teleport_spec = bits[3].split("+")
-                tile.setTeleporter(True, teleport_spec[0], teleport_spec[1])
-                self.scene.addPixmap(QPixmap(
-                    "./images/teleporters/teleport_" + teleport_spec[0] + ".png")).setOffset(int(bits[2])*20-5, int(bits[1])*20-5)
+                self.tiles[int(bits[1])][int(bits[2])].setTeleporter(
+                    True, teleport_spec[0], teleport_spec[1], self.scene)
                 continue
 
             if line[:5] == "BLOCK":
@@ -56,36 +53,19 @@ class Maze(QWidget):
 
             if line[:4] == "LINK":
                 bits = line.split(" ")
-                tile = self.tiles[int(bits[-2])][int(bits[-1])]
-                link_spec = bits[1:3]
-                tile.setLink(True, link_spec[0], link_spec[1])
-                self.blocks[link_spec[0]].add_exit(
-                    link_spec[1], int(bits[-2]), int(bits[-1]))
-                if self.exits[link_spec[1]].orientation == 1:
-                    orientation = 3
-                elif self.exits[link_spec[1]].orientation == 2:
-                    orientation = 4
-                elif self.exits[link_spec[1]].orientation == 3:
-                    orientation = 1
-                elif self.exits[link_spec[1]].orientation == 4:
-                    orientation = 2
-                else:
-                    orientation = -1
-                self.scene.addPixmap(QPixmap(
-                    "./images/links/link_"+str(orientation)+".png")).setOffset(int(bits[-1])*20-5, int(bits[-2])*20-5)
+                exit_tile = self.tiles[self.exits[bits[2]]
+                                       [0]][self.exits[bits[2]][1]]
+                self.tiles[int(bits[-2])][int(bits[-1])].setLink(
+                    True, bits[1], exit_tile, self.scene)
+                self.blocks[bits[1]].add_exit(
+                    bits[2], int(bits[-2]), int(bits[-1]))
                 continue
 
             if line[:4] == "EXIT":
                 bits = line.split(" ")
-                tile = self.tiles[int(bits[-2])][int(bits[-1])]
-                exit_spec = bits[1:3]
-                self.exits[exit_spec[0]] = Exit(exit_spec[0], exit_spec[1], int(
-                    bits[-2]), int(bits[-1]))
-                tile.setExit(True, exit_spec[0])
-                self.exits[exit_spec[0]].setPixmap(self.scene.addPixmap(QPixmap(
-                    "./images/exits/exit_" + str(self.exits[exit_spec[0]].orientation) + ".png")))
-                self.exits[exit_spec[0]].pixmap.setOffset(
-                    int(bits[-1])*20-5, int(bits[-2])*20-5)
+                self.tiles[int(bits[-2])][int(bits[-1])
+                                          ].setExit(True, bits[1], bits[2], self.scene)
+                self.exits[bits[1]] = (int(bits[-2]), int(bits[-1]))
                 continue
 
             if line[:6] == "PLAYER":
@@ -94,15 +74,11 @@ class Maze(QWidget):
                     int(player_init[2]), int(player_init[1]), self.scene)
                 continue
 
-            row = []
-            for j in range(len(line) >> 1):
-                tile = Tile(i, j, line[2*j:2*j+2])
-                self.scene.addPixmap(tile.pixmap).setOffset(j*20, i*20)
-                row += [tile]
+            self.tiles += [[Tile(i, j, line[2*j:2*j+2], self.scene)
+                            for j in range(len(line) >> 1)]]
             i += 1
-            self.tiles += [row]
 
-    @Slot()
+    @ Slot()
     def reset(self):
         if not self.win:
             self.player.reset()
@@ -140,21 +116,23 @@ class Maze(QWidget):
                     self.player.x += tile.teleport_reach
 
             if tile.is_link:
-                north = self.exits[tile.link_name].orientation == 2
-                east = self.exits[tile.link_name].orientation == 3
-                south = self.exits[tile.link_name].orientation == 4
-                west = self.exits[tile.link_name].orientation == 1
+                exit_tile = self.tiles[self.exits[tile.link_name]
+                                       [0]][self.exits[tile.link_name][1]]
+                north = exit_tile.exit_orientation == 2
+                east = exit_tile.exit_orientation == 3
+                south = exit_tile.exit_orientation == 4
+                west = exit_tile.exit_orientation == 1
                 if (key == Qt.Key_Z and north) or (key == Qt.Key_Q and west) or (key == Qt.Key_S and south) or (key == Qt.Key_D and east):
-                    self.player.x = self.exits[tile.link_name].y
-                    self.player.y = self.exits[tile.link_name].x
+                    self.player.x = exit_tile.y
+                    self.player.y = exit_tile.x
                     self.block_changed(new_block=tile.block_name)
                     animate = False
 
             if tile.is_exit:
-                north = self.exits[tile.exit_name].orientation == 4
-                east = self.exits[tile.exit_name].orientation == 1
-                south = self.exits[tile.exit_name].orientation == 2
-                west = self.exits[tile.exit_name].orientation == 3
+                north = tile.exit_orientation == 4
+                east = tile.exit_orientation == 1
+                south = tile.exit_orientation == 2
+                west = tile.exit_orientation == 3
                 if (key == Qt.Key_Z and north) or (key == Qt.Key_Q and west) or (key == Qt.Key_S and south) or (key == Qt.Key_D and east):
                     if self.block_stack[-1] != '0':
                         current_block = self.blocks[self.block_stack[-1]]
@@ -171,6 +149,7 @@ class Maze(QWidget):
                         self.view.fitInView(
                             self.scene.itemsBoundingRect(), Qt.KeepAspectRatio)
                         self.game_over.emit(False)
+                        return
 
             self.player.move(animate)
 
@@ -182,12 +161,14 @@ class Maze(QWidget):
 
         if self.block_stack[-1] == '0':
             for exit in self.exits.values():
-                exit.show()
+                self.tiles[exit[0]][exit[1]].showExit()
         else:
             current_block = self.blocks[self.block_stack[-1]]
             for exit in self.exits.keys():
                 if exit in current_block.exits.keys():
-                    self.exits[exit].show()
+                    self.tiles[self.exits[exit][0]
+                               ][self.exits[exit][1]].showExit()
                 else:
-                    self.exits[exit].hide()
+                    self.tiles[self.exits[exit][0]
+                               ][self.exits[exit][1]].hideExit()
         self.change_block.emit(self.block_stack)
--- tiles.py
+++ tiles.py
@@ -1,35 +1,57 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import QObject
 from PySide6.QtGui import QPixmap
 
 
-class Tile(QWidget):
-    def __init__(self, row, col, tile_type):
-        QWidget.__init__(self)
-        self.row = row
-        self.col = col
+class Tile(QObject):
+    def __init__(self, x, y, tile_type, scene):
+        QObject.__init__(self)
+        self.x = x
+        self.y = y
         self.type = int(tile_type)
         self.is_teleport = False
         self.is_link = False
         self.is_exit = False
+        self.background = scene.addPixmap(
+            QPixmap("./images/tiles/tile_" + tile_type + ".png"))
+        self.background.setOffset(y*20, x*20)
 
-        self.pixmap = QPixmap("./images/tiles/tile_" + tile_type + ".png")
-
-    def setTeleporter(self, is_teleport, direction=-1, reach=-1):
+    def setTeleporter(self, is_teleport, direction=None, reach=None, scene=None):
         self.is_teleport = is_teleport
         if not self.is_teleport:
             return
         self.teleport_direction = int(direction)
         self.teleport_reach = int(reach)
+        self.teleporter = scene.addPixmap(QPixmap(
+            "./images/teleporters/teleport_" + direction + ".png"))
+        self.teleporter.setOffset(self.y*20-5, self.x*20-5)
 
-    def setExit(self, is_exit, exit_name=-1):
+    def setExit(self, is_exit, exit_name=None, orientation=None, scene=None):
         self.is_exit = is_exit
         if not self.is_exit:
             return
         self.exit_name = exit_name
+        self.exit = scene.addPixmap(QPixmap(
+            "./images/exits/exit_" + orientation + ".png"))
+        self.exit.setOffset(self.y*20-5, self.x*20-5)
+        self.exit_orientation = int(orientation)
 
-    def setLink(self, is_link, block_name="", link_name=""):
+    def setLink(self, is_link, block_name=None, exit_tile=None, scene=None):
         self.is_link = is_link
         if not self.is_link:
             return
         self.block_name = block_name
-        self.link_name = link_name
+        self.link_name = exit_tile.exit_name
+        orientation = exit_tile.exit_orientation + 2
+        if orientation > 4:
+            orientation -= 4
+        self.link = scene.addPixmap(QPixmap(
+            "./images/links/link_"+str(orientation)+".png"))
+        self.link.setOffset(self.y*20-5, self.x*20-5)
+
+    def hideExit(self):
+        if self.is_exit:
+            self.exit.hide()
+
+    def showExit(self):
+        if self.is_exit:
+            self.exit.show()
