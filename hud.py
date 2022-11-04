f84a1e6c6f6db346ca8c2b1dc1c2c173aaab744c
Orel Cosseron <orel.cosseron@ens-lyon.fr>
2022-11-04 10:58:42 +0100

    Hud is even more minimalistic

--- hud.py
+++ hud.py
@@ -5,29 +5,21 @@ from PySide6.QtWidgets import (QLabel, QHBoxLayout, QWidget)
 class Hud(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-
-        self.depth = QLabel("Depth: 0")
-        self.block = QLabel("Position: 0")
+        self.path = QLabel("Position: 0")
 
         self.layout = QHBoxLayout(self)
 
-        self.layout.addWidget(self.depth)
-        self.layout.addWidget(self.block)
+        self.layout.addWidget(self.path)
 
     @Slot()
     def reset(self):
-        self.depth.setText("Depth: 0")
-        self.block.setText("Position: 0")
+        self.path.setText("Position: 0")
 
     @Slot()
     def update(self, stack):
-        self.depth.setText("Depth: " + str(len(stack)-1))
-        str_stack = "Position: 0"
-        for block in stack[1:]:
-            str_stack += " <- " + block
-        self.block.setText(str_stack)
+        stack[0] = "Position: 0"
+        self.path.setText(" <- ".join(stack))
 
     @Slot()
     def game_over(self, b):
-        self.depth.setText("")
-        self.block.setText("Congratulations, you escaped!")
+        self.path.setText("Congratulations, you escaped!")
